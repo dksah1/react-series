acbc5e5280823a37c25642253b0ab0d5ef962647
dksah1 <dkboss127126@gmail.com>
2023-10-25 03:26:06 +0545

    config

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -1,3 +1,29 @@
 import conf from "../conf.js";
-import { Client, Account, ID } from "appwrite";
+import { Client,ID,Databases,Storage,Query } from "appwrite";
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.client
+        .setEndpoint(conf.API_ENDPOINT)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+    async createPost({title,slug,content,featuredImage,
+        status,userId}) {
+            try {
+                
+            } catch (error) {
+                console.log("Appwrite service :: createPost :: error", error);
+            }
+        }
+     
+}
+
+
+const service = new Service()
+export default service
 
