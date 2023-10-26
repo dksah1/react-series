4fa6d7afc1e97709079c5d4a476c32a94c618450
dksah1 <dkboss127126@gmail.com>
2023-10-27 03:18:07 +0545

    second pages

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -9,7 +9,7 @@ import Login from './Login';
 import Post from "./Post"
 import PostForm from "./post-form/PostForm"
 import PostCard from "./PostCard"
-
+import AuthLayout from "./AuthLayout"
 export {
     Header,
     Footer,
@@ -22,4 +22,5 @@ export {
     Post,
     PostForm,
     PostCard,
+    AuthLayout
 }
--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -4,7 +4,32 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import AuthLayout from './components/AuthLayout.jsx'
+import Login from './components/Login.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path:'/',
+        element:<Home/>,
+      },
+      {
+        path:"/login",
+        element:(
+          <AuthLayout>
+            <Login/>
+          </AuthLayout>
+        )
+      }
+    ]
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
